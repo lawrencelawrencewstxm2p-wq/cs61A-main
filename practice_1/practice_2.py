233659b80c5e5b2620d24bb81b6a49d30f3a8ebb
lawrencelawrencewstxm2p-wq <lawrencelawrencewstxm2p@gmail.com>
2026-07-10 18:09:50 +0800

    lab02

--- practice_1/practice_2.py
+++ practice_1/practice_2.py
@@ -1,4 +1,4 @@
-def square(x):
+'''def square(x):
     return x*x
 def triple(x):
     return 3*x
@@ -18,3 +18,44 @@ def f(g):
     a=2
     return lambda y:a * g(y)
 print(f(lambda y: a+y)(a))
+'''
+
+def sum_all(x):
+    print(x)
+    def next_sum(y):
+        return sum_all(x+y)
+    return next_sum
+
+sum_all(1)(2)(3)
+
+f1 = sum_all(1)
+f2 = f1(2)
+f3 = f2(3)
+
+def split(n):
+    return n//10 , n%10
+
+def get_sum(n):
+    if n <= 9:
+        return n
+    else:
+        allbutlast,last=split(n)
+        return get_sum(allbutlast) + last
+
+def luhn_sum(n):
+    if n <= 9:
+        return n
+    else:
+        allbutlast,last = split(n)
+        return luhn_double(allbutlast) + last
+    
+def luhn_double(n):
+    allbutlast,last = split(n)
+    nlast_double = last*2
+    
+    return luhn_sum(allbutlast) + get_sum(nlast_double)
+
+print(luhn_sum(2))
+print(luhn_sum(322))
+print(luhn_sum(32))
+print(luhn_sum(5105105105105100))
